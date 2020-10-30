f4a969e5c1544cbd28c7efd6c7819b2b109bb2c1
William Pannell <lpann5@gmail.com>
2020-10-30 21:19:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,7 +11,6 @@ def won?(board)
     win1 = board[x[1]]
     win2 = board[x[2]]
     win3 = board[x[3]]
-
     if (win1 == "X" || win1 == "O") && (win2 == "X" || win2 == "O") && (win3 == "X" || win3 == "O")
       return x
     end
