4c20895a66d4187857fabffd4437ee6d8573562b
William Pannell <lpann5@gmail.com>
2020-10-30 21:28:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,12 +8,12 @@ WIN_COMBINATIONS =[ [0,1,2] , [3,4,5] , [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , 
 
 def won?(board)
   WIN_COMBINATIONS.each do |x|
-    x[1]
-    x[2]
-    x[3]
-    win1 = board[]
-    win2 = board[]
-    win3 = board[]
+    row = x[1]
+    row2 = x[2]
+    row3 = x[3]
+    win1 = board[row]
+    win2 = board[row2]
+    win3 = board[row3]
 
     if (win1 == "X" || win1 == "O") && (win2 == "X" || win2 == "O") && (win3 == "X" || win3 == "O")
       return x
