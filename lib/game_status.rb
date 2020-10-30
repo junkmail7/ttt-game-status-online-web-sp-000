9c11b3cf900feab1d4f98aaac8969af531953779
William Pannell <lpann5@gmail.com>
2020-10-30 21:24:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,19 +19,19 @@ WIN_COMBINATIONS =[ [0,1,2] , [3,4,5] , [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , 
 #  return false
 #end
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+  WIN_COMBINATIONS.each do |x|
+    index_0 = x[0]
+    index_1 = x[1]
+    index_2 = x[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
+      return x
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+      return x
     end
   }
   return false
