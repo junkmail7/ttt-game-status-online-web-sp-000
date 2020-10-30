cbd246b96087c2a660a7f77b0c362008b27c233f
William Pannell <lpann5@gmail.com>
2020-10-30 21:23:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,16 +6,34 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS =[ [0,1,2] , [3,4,5] , [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , [0,4,8] , [2,4,6] ]
 
-def won?(board)
-  WIN_COMBINATIONS.each do |x|
-    win1 = board[x[1]]
-    win2 = board[x[2]]
-    win3 = board[x[3]]
+#def won?(board)
+#  WIN_COMBINATIONS.each do |x|
+#    win1 = board[x[1]]
+#    win2 = board[x[2]]
+#    win3 = board[x[3]]
 
-    if (win1 == "X" || win1 == "O") && (win2 == "X" || win2 == "O") && (win3 == "X" || win3 == "O")
-      return x
+#    if (win1 == "X" || win1 == "O") && (win2 == "X" || win2 == "O") && (win3 == "X" || win3 == "O")
+#      return x
+#    end
+#  end
+#  return false
+#end
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
-  end
+  }
   return false
 end
 
